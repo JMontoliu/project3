d394aef2d4185c167996da39bba2b93b893d3fce
jmolto10 <132159693+molti10@users.noreply.github.com>
2025-05-12 19:05:25 +0200

    Revert "langchain to -> langgraph"

--- chatbot/telegram_edem.py
+++ chatbot/telegram_edem.py
@@ -1,32 +1,38 @@
-# main.py
-
 import os
 from telegram import Update
 from telegram.ext import ContextTypes, Application, CommandHandler, MessageHandler, filters
-from llm_assistant import create_chain  # ahora importa desde chatbot.py
-from dotenv import load_dotenv
+from llm_assistant import create_chain
 
+# Establecer la variable de entorno en tiempo de ejecución
+os.environ["TELEGRAM_API_KEY"] = ""
+
+# Diccionario para guardar memoria por usuario
 user_chains = {}
-load_dotenv()
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
         "Hola! Soy un asistente virtual para reservar. ¿Cómo te puedo ayudar?"
     )
 
+
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.effective_user.id
 
+    # Si no existe, crear nueva memoria y cadena para ese usuario
     if user_id not in user_chains:
         user_chains[user_id] = create_chain()
 
     chain = user_chains[user_id]
     user_input = update.message.text
-    response = chain.invoke(user_input)
-    await update.message.reply_text(response.strip())
+    response = chain.invoke({"new_input": user_input})
+    await update.message.reply_text(response["text"].strip())
+
 
 def run_bot():
+    os.environ['GOOGLE_API_KEY'] = ""
     application = Application.builder().token(os.getenv("TELEGRAM_API_KEY")).build()
+
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
