904395afb7be6b75b628e9c7721053caecd31be6
Juan Montoliu <juan00montoliu@gmail.com>
2025-05-17 16:11:31 +0200

    Revert "injectors"

--- terraform/modules/api/output.tf
+++
@@ -1,4 +0,0 @@
-output "api_url" {
-  description = "URL API"
-  value       = google_cloud_run_v2_service.service.uri
-}
--- terraform/modules/injectors/main.tf
+++
@@ -1,8 +0,0 @@
-resource "null_resource" "update-api-agent" {
-
-  provisioner "local-exec" {
-      command = <<EOT
-      gcloud run services update ${var.chatbot} --region=${var.region} --project=${var.project_id} --update-env-vars=POST_API_URL=${var.api_url}
-  EOT
-    }
-}
